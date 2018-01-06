e6cf671619696ba5f6b4042f3972687842a9fbfd
ardraunnikrishnan <ardraunnikrishnan95@gmail.com>
2018-01-06 14:55:29 +0530

    php program

---
+++ al.php
@@ -0,0 +1,48 @@
+<?php
+
+
+
+	if(isset($_FILES['image']))
+	{
+      $errors= array();
+      $file_name = $_FILES['image']['name'];
+      $file_size =$_FILES['image']['size'];
+      $file_tmp =$_FILES['image']['tmp_name'];
+      $file_type=$_FILES['image']['type'];
+      $image=$_FILES['image']['name'];
+      $file_e=explode('.',$image);
+      $file_ext=$file_e[1];
+     
+      
+      $extensions= array("jpeg","jpg","png");
+      
+      if(in_array($file_ext,$extensions)=== false){
+         $errors[]="extension not allowed, please choose a JPEG or PNG file.";
+      }
+      
+      if($file_size > 2097152){
+         $errors[]='File size must be excately 4 MB';
+      }
+      
+      if(empty($errors)==true){
+         move_uploaded_file($file_tmp,"upload/".$file_name);
+         echo "Success";
+      }else{
+         print_r($errors);
+      }
+   }
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<form enctype="multipart/form-data" action="" method="post">
+	<input type="file" name="image"></input>
+	<input type="submit" value="upload"></input>
+</form>
+</body>
+</html>
